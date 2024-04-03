5f081882cdf0d741e7c7fe8c7911919bdb8b49a6
Build Helper <100530341+BRINNOVATIONS@users.noreply.github.com>
2024-04-03 21:42:46 +0530

    Update domain-check.js

--- Tetris/domain-check.js
+++ Tetris/domain-check.js
@@ -1,17 +1,1 @@
-var host=document.location.href;
-var referrer=document.referrer;
-var testUserAgent=navigator.userAgent;
-var gameInTesting=[];
-var isTesting=(isTesting)?isTesting:false;
-var gameName=(gameName)?gameName:"";
 
-if(testUserAgent.search("Vinebre")>=0||            	
-    referrer.search("e-droid.net")>=0||referrer.search("appcreator24.com")>=0||
-    referrer.search("cloudfront.net")>=0||referrer.search("amazonaws.com")>=0||location.href.search("marketjs-cloud2.com")>=0)
-{
-	//FOR TESTING LINK
-}
-else if(isTesting&&gameInTesting.indexOf(gameName)>=0)
-	alert("Testing. This should be redirect");
-else
-	window.location.href = "https://brinnovations.github.io/Tetris/access-denied.html";
